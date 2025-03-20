1130b2c2017371dbf78c3929c951d8717dcee733
AnnetteCarolyn <aneta.translation@gmail.com>
2025-03-20 14:33:47 +0100

    Initial commit

---
+++ Aneta's Word Counting Console/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Aneta_s_Word_Counting_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            Console.WriteLine("Twoje słowo to:");
+            string s =  Console.ReadLine();
+                
+                
+            //Console.WriteLine("The number of letters in your word(s) is:" + s.Length) ;
+            Console.WriteLine($"The number of letters in your word(s) is: {s.Length}");
+
+
+
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+
+            Console.WriteLine(charArray);
+
+            string result = s.ToUpper();
+            Console.WriteLine($"Original String : {s}"); //=>  Console.WriteLine("Original String :" + " " + s)
+            Console.WriteLine(result);
+
+
+
+            for (int i = 0; i < charArray.Length; i++) 
+            {
+                Console.WriteLine(charArray[i]);
+            }
+
+
+        }
+        
+       
+        
+    }
+}
